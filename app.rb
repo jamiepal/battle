b2c7617d204df5407175103cf62fd938aa30d980
TBrockster <tobybrock93@gmail.com>
2019-07-11 15:46:06 +0100

    controllers are now skinny

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra/base'
 require_relative './lib/player'
+require_relative './lib/game'
 
 # this is the battle class
 class Battle < Sinatra::Base
@@ -16,21 +17,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:name1])
-    $player_2 = Player.new(params[:name2])
+    player_1 = Player.new(params[:name1])
+    player_2 = Player.new(params[:name2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @name1 = $player_1
-    @name2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @name1 = $player_1
-    @name2 = $player_2
-    Game.new.attack(@name2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
   end
   run if app_file == $PROGRAM_NAME
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -3,9 +3,21 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(toby, james) }
   let(:toby) { double :fake_player }
   let(:james) { double :fake_player }
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq toby
+    end
+  end
+  
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq james
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
